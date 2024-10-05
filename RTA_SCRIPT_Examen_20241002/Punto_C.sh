ad332514b832af43f392ecd5efb7bc00d2336bc5
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-05 15:18:22 +0000

    feat: Punto C terminado

--- RTA_SCRIPT_Examen_20241002/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -24,3 +24,9 @@ sudo chmod 730 /Examenes-UTN/alumno_1
 sudo chmod 760 /Examenes-UTN/alumno_2
 sudo chmod 700 /Examenes-UTN/alumno_3
 sudo chmod 773 /Examenes-UTN/profesores
+
+sudo su -c "whoami" p1c2_2024_A1 | sudo tee /Examenes-UTN/alumno_1/validar1.txt
+sudo su -c "whoami" p1c2_2024_A2 | sudo tee /Examenes-UTN/alumno_2/validar2.txt
+sudo su -c "whoami" p1c2_2024_A3 | sudo tee /Examenes-UTN/alumno_3/validar3.txt
+sudo su -c "whoami" p1c2_2024_P1 | sudo tee /Examenes-UTN/profesores/profesores.txt
+
