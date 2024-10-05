7f834dec13dc61aa5a6343184a55681f55968ddc
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-05 04:56:42 +0000

    ADD: Punto B finalizado, comienzo punto C

--- RTA_SCRIPT_Examen_20241002/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_B.sh
@@ -4,7 +4,8 @@
 
 DISCO="/dev/sdc"
 
-sudo fdisk $DISCO << EOF
+#ARRANCAMOS A PARTICIONAR, COLOCANDO DENTRO DEL EOF, LAS LETRAS Y MONTOS QUE DEBEMOS COLOCAR EN LAS PARTICIONES
+sudo fdisk $DISCO << EOF   
 n
 p
 
@@ -47,35 +48,48 @@ n
 +900M
 
 w
-#####DISCO PARTICIONADO#
-EOF
 
-for i in {1..11}
+EOF
+#ACA TERMINAMOS EL PARTICIONAMIENTO
+
+for i in {1..11} #REALIZAMOS UN FOR, QUE VAYA PASANDO POR CADA PARTICION, UTILIZANDO EL VALOR i COMO NUMERO DE PARTICION
 do
+#UTILIZAMOS EL EOF PARA PONER LA y QUE NOS VA A PERMITIR DARLE QUE SI AUTOMATICAMENTE A LA HORA DE PARTICIONAR
 sudo mkfs.ext4 /dev/sdc$i <<EOF
 y
-EOF
+EOF 
 done
 
 set -x
 
+#CREAMOS UNA VARIABLE, CON VALOR 1, QUE VA A TOMAR EL PAPEL DE CONTADOR. ESTO PARA USARLO COMO NUMERO DE PARTICION MAS ABAJO
 particion=1
 
-for i in {1..3}
+for i in {1..3} #ESTE FOR RECORRE LA CARPETA ALUMNOS, UNA VEZ QUE TERMINA CON LOS PARCIALES, VUELVE Y SUMA 1, PASANDO DE alumno_1 A alumno_2, Y ASI CON TODOS
 do
 
-	for j in {1..3}
+	for j in {1..3} #ESTE FOR RECORRE LOS PARCIALES, PARCIALES, CUANDO TERMINA DE RECORRER LOS 3, SALE PARA QUE ALUMNO SUME 1
 	do
 
-		if [[ $particion == 4 ]]; then
+		if [[ $particion == 4 ]]; then #ESTE IF SE SALTEA LA PARTICION 4, LA MISMA ES UNA EXTENDIDA, LA CUAL NO ES POSIBLE MONTAR, PERO SI SE MONTARAN LAS LOGICAS DENTRO DE ESTA
 			((particion++))
 		fi
-	echo "/dev/sdc${particion} Examenes-UTN/alumno_${i}/parcial_${j} ext4 defaults 0   0 " | sudo tee -a /etc/fstab
+	echo "/dev/sdc${particion} /Examenes-UTN/alumno_${i}/parcial_${j} ext4 defaults 0   0 " | sudo tee -a /etc/fstab #ESTE ES EL COMANDO PARA ESCRIBIR DONDE SE MONTARAN LAS PARTICIONES EN EL ARCHIVO /etc/fstab
 
-	((particion++)) 
+	((particion++))  
 
         done
 done
 
-echo "/dev/sdc${particion} Examenes-UTN/profesores ext4 defaults 0   0 " | sudo tee -a /etc/fstab
+echo "/dev/sdc${particion} /Examenes-UTN/profesores ext4 defaults 0   0 " | sudo tee -a /etc/fstab  #AQUI, LA PARTICION QUE FALTA, SE AVISARA QUE SE MONTE EN LA CARPETA PROFESORES
 
+
+<<EOF
+#######################AGREGAMOS AL ARCHIVO /dev/sdc LAS PARTICIONES#############################################
+EOF
+
+sudo mount -a #MONTAMOS LO QUE SE ENCUENTRA EN LA CARPETA /etc/fstab
+
+<<EOF
+################################MONTAMOS LAS PARTICIONES#########################################################
+EOF
---
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ALUMNOS=p1c2_2024_A
+PROFESORES=p1c2_2024_P1
+
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
+
+for i in {1..3}
+do
+    sudo useradd -m -s /bin/bash -c "$ALUMNOS$i" -g p1c2_2024_gAlumno $ALUMNOS$i
+    echo "${ALUMNOS}${i}:${ALUMNOS}${i}" | sudo chpasswd
+
+done
+sudo useradd -m -s /bin/bash -c "$PROFESORES" -g p1c2_2024_gProfesores $PROFESORES
+echo "${PROFESORES}:${PROFESORES}" | sudo chpasswd
+
+sudo chown p1c2_2024_A1  /Examenes-UTN/alumno_1
+sudo chown p1c2_2024_A2  /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3  /Examenes-UTN/alumno_3
+sudo chown ${PROFESORES} /Examenes-UTN/profesores
+
+sudo chmod 730 /Examenes-UTN/alumno_1
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chmod 773 /Examenes-UTN/profesores
