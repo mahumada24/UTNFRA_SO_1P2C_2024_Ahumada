c40b8cb2baf21a28c5f943a87fee41868eabf41e
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-07 03:37:27 +0000

    feat: Punto D finalizado

--- RTA_SCRIPT_Examen_20241002/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_D.sh
@@ -4,3 +4,4 @@ USUARIO=$(whoami) #Creamos una variable que contenga el usuario
 
 #Creamos en el home, una carpeta que contenga lo solicitado en el examen, con el -p para que, si no existe la direccion, se cree.
 mkdir -p /home/${USUARIO}/Estructura_Asimetrica/{correo/{cartas{1..100},carterps{1..10}},clientes/cartas{1..100}}
+
