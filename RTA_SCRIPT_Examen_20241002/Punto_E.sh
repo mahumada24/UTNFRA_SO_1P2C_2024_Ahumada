f242efe40bcf2a164dc7dd2ffb12056b9d328b5f
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-07 03:38:40 +0000

    fix: Punto E corregido y finalizado

--- RTA_SCRIPT_Examen_20241002/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_E.sh
@@ -5,3 +5,4 @@ touch ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_
 head -n 1 /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
 #Realizamos la escritura de la informacion del chasis solicitada
 sudo dmidecode -t chassis | head -n 7 | tail -n 2 >> ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
+
