38b1f5363a0a26152d49d272dd9379b1e76eebbd
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-07 19:14:21 +0000

    fix: Corregimos error de directorio en el Punto_F

--- RTA_SCRIPT_Examen_20241002/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_F.sh
@@ -3,7 +3,8 @@ echo "Mi IP publica es: $(curl -s ifconfig.me)" | tee -a ~/repogit/UTNFRA_SO_1P2
 
 echo "Mi usuario es: $(whoami)" | tee -a ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt
 
-echo -n "El hash de mi Usuario es: " | sudo tee -a UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt && sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}' | tee -a UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt
+
+echo -n "El hash de mi Usuario es: " | sudo tee -a ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt && sudo grep $(whoami) /etc/shadow | sudo awk -F ':' '{print $2}' | sudo tee -a ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt
 
 cd ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada && echo -n "La URL de mi repositorio es: $(git remote get-url origin)" |tee -a ~/repogit/UTNFRA_SO_1P2C_2024_Ahumada/RTA_ARCHIVOS_Examen_20241005/Filtro_Avanzado.txt
 
