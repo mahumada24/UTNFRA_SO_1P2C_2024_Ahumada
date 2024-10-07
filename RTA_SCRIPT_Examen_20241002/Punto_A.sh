c9ee83bdc880dea20e7a9cc2128b0146e2e4c21f
mahumada24 <marcos-ahumada@hotmail.com>
2024-10-07 03:30:48 +0000

    feat: Punto A terminado

--- RTA_SCRIPT_Examen_20241002/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_A.sh
@@ -5,6 +5,7 @@
 
 #El script crea varias carpetas, el -p se usa para que, si no esta creada, la cree en ese momento.
 #El sudo fue usado ya que, normalmente, en la carpeta raiz "/", solo los superusuarios pueden realizar cambios
+
 sudo mkdir -p /Examenes-UTN/{alumno_1/parcial_{1..3},alumno_2/parcial_{1..3},alumno_3/parcial_{1..3},profesores}
 
 
